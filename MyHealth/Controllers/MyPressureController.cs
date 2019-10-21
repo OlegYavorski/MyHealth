16aee85f8f1991725417e33ade1fb48d05195a50
Oleg Yavorsky <mw17@ukr.net>
2019-10-21 18:18:39 +0300

    Edit view added

--- MyHealth/Controllers/MyPressureController.cs
+++ MyHealth/Controllers/MyPressureController.cs
@@ -1,6 +1,7 @@
 ﻿using MyHealth.Models;
 using System;
 using System.Web.Mvc;
+using System.Linq;
 
 
 namespace MyHealth.Controllers
@@ -38,16 +39,49 @@ namespace MyHealth.Controllers
             return View();
         }
 
+        public ActionResult Edit(int id)
+        {
+            var item = _context.MyPressures.SingleOrDefault(c => c.Id == id);
+
+            if (item == null)
+                return HttpNotFound();
+
+            return View("Edit", item);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var item = _context.MyPressures.SingleOrDefault(c => c.Id == id);
+
+            if (item == null)
+                return HttpNotFound();
+
+            return View(item);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Save(MyPressure myPressure)
+        public ActionResult DeleteConfirmed(MyPressure myPressure)
         {
             if (!ModelState.IsValid)
             {
-                return View("Create");
+                return View("Error");
             }
 
-            _context.MyPressures.Add(myPressure);
+            if (myPressure.Id == 0)
+            {
+                _context.MyPressures.Add(myPressure);
+            }
+            else
+            {
+                var myPressureInDb = _context.MyPressures.Single(m => m.Id == myPressure.Id);
+                myPressureInDb.High = myPressure.High;
+                myPressureInDb.Low = myPressure.Low;
+                myPressureInDb.When = myPressure.When;
+                myPressureInDb.TonometerId = myPressure.TonometerId;
+                myPressureInDb.PersonId = myPressure.PersonId;
+                myPressureInDb.Comment = myPressure.Comment;
+            }
 
             try
             {
@@ -57,7 +91,42 @@ namespace MyHealth.Controllers
             {
                 throw;
             }
-            return RedirectToAction("Index", "MyPressures");
+            return RedirectToAction("List", "MyPressure");
+        }
+
+    [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(MyPressure myPressure)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create");
+            }
+
+            if (myPressure.Id == 0)
+            {
+                _context.MyPressures.Add(myPressure);
+            }
+            else
+            {
+                var myPressureInDb = _context.MyPressures.Single(m => m.Id == myPressure.Id);
+                myPressureInDb.High = myPressure.High;
+                myPressureInDb.Low = myPressure.Low;
+                myPressureInDb.When = myPressure.When;
+                myPressureInDb.TonometerId = myPressure.TonometerId;
+                myPressureInDb.PersonId = myPressure.PersonId;
+                myPressureInDb.Comment = myPressure.Comment;
+            }
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return RedirectToAction("List", "MyPressure");
         }
        
     }
