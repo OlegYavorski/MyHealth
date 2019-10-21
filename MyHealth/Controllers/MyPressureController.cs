d13a20f203addeef898ddce9d8ddc8849d881fb0
Oleg Yavorsky <mw17@ukr.net>
2019-10-21 11:49:18 +0300

    CS Project added

---
+++ MyHealth/Controllers/MyPressureController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MyHealth.Controllers
+{
+    public class MyPressureController : Controller
+    {
+        // GET: MyPressure
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyHealth/Models/MyPressure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MyHealth.Models
+{
+    public class MyPressure
+    {
+        [Required]
+        public int High { get; set; }
+
+        [Required]
+        public int Low { get; set; }
+
+        [Required]
+        public DateTime When { get; set; }
+
+        [Required]
+        public int Tonometer { get; set; }
+
+        [Required]
+        public int Person { get; set; }
+
+        [StringLength(255)]
+        public string Comment { get; set; }
+    }
+}
